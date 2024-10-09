99cdcd309f28b21562154ff30e7bc3c9cf1f3ffa
Torahime3 <nathan.dulac@epitech.eu>
2024-10-09 16:01:28 +0200

    feat(UserComponent): Init the component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+import UserComponent from "./components/UserComponent.vue";
 </script>
 
 <template>
   <header></header>
-
+  <UserComponent />
   <RouterView />
 </template>
