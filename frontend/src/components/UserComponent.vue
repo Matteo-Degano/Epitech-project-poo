3266637f44ce92a2da81eb43e94623a7212eda0c
Torahime3 <nathan.dulac@epitech.eu>
2024-10-09 16:01:28 +0200

    feat(UserComponent): Init the component

---
+++ frontend/src/components/UserComponent.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const user = ref({
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+})
+
+</script>
+
+<template>
+  <div class="bg-slate-800 p-2 rounded-md">
+    <h1 class="font-bold">Current user you are connected on </h1>
+    <p>Username : {{ user.name }}</p>
+    <p>Email : {{ user.email }}</p>
+  </div>
+</template>
