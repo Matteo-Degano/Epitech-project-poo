797a17c6559938fe8ca8bdad16fffe8c22429a45
Torahime3 <nathan.dulac@epitech.eu>
2024-10-09 21:58:08 +0200

    feat(UserComponent): Added a way to create, get, update and delete user

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import UserComponent from "./components/UserComponent.vue";
 </script>
 
 <template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UserComponent from "../components/UserComponent.vue";
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <UserComponent />
+  </main>
 </template>
