9492a91a24cc43d33993be9aef4d0971596996d4
LoubatTh <thomas.loubat@epitech.eu>
2024-10-09 11:00:09 +0200

    feat: crud user

--- backend/lib/api_web/router.ex
+++ backend/lib/api_web/router.ex
@@ -28,6 +28,8 @@ defmodule ApiWeb.Router do
 
     # DELETE - Delete a working time entry
     delete "/workingtime/:id", WorkingtimeController, :delete
+
+    resources "/users", UserController, except: [:new, :edit]
   end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
