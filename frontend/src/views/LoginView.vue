3182335560380ad772ebf0841352fae2943e09b6
Matteo Degano <mdegano.webdev@gmail.com>
2024-10-16 16:04:10 +0200

    added logo on view

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -2,11 +2,15 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import LoginComponent from "@/components/authentification/LoginComponent.vue"
 import SigninComponent from "@/components/authentification/SigninComponent.vue"
+import logo from "@/assets/logo.png"
 </script>
 
 <template>
   <main class="w-screen h-screen">
-    <div class="flex w-full h-full justify-center mt-52">
+    <div class="flex flex-col w-full h-full items-center gap-10 mt-14">
+      <div>
+        <img :src="logo" alt="App Logo" class="w-96 my-auto" />
+      </div>
       <Tabs default-value="login" class="w-[600px]">
         <TabsList class="grid w-full grid-cols-2">
           <TabsTrigger value="login"> Login </TabsTrigger>
