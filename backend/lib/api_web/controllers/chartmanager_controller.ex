4786dbe39faafb59906f5d6f464c882ac37ab5e5
Torahime3 <nathan.dulac@epitech.eu>
2024-10-23 10:16:59 +0200

    feat(chartmanager): Added the night times worked to the chart 3

--- backend/lib/api_web/controllers/chartmanager_controller.ex
+++ backend/lib/api_web/controllers/chartmanager_controller.ex
@@ -26,15 +26,18 @@ defmodule ApiWeb.ChartManagerController do
 
           workingtimes = Workingtimes.list_workingtimes_by_start_and_end(workingtime_params)
 
-          donut_chart_data = calc_donut_chart(start_date, end_date, workingtimes)
-          line_chart_data = calc_line_chart(workingtimes)
+          chart_1_data = calc_chart_1(start_date, end_date, workingtimes)
+          chart_2_data = calc_chart_2(workingtimes)
+
+          chart_3_data = calc_chart_3(workingtimes)
 
           response = %{
             start_date: DateTime.to_iso8601(start_date),
             end_date: DateTime.to_iso8601(end_date),
             total_days: Date.diff(end_date, start_date),
-            donut_chart: donut_chart_data,
-            line_chart: line_chart_data
+            # chart_3: chart_3_data,
+            # chart_1: chart_1_data,
+            # chart_2: chart_2_data
           }
 
           conn
@@ -57,10 +60,64 @@ defmodule ApiWeb.ChartManagerController do
 
   end
 
-  # Calcule le temps travaillé par jour en secondes pour chaque jour de la période
-  def calc_line_chart(workingtimes) do
 
-    # On récupère la différence entre la date de début et la date de fin de travail pour en extarire les secones
+  def calc_chart_3(workingtimes) do
+    data = Enum.map(workingtimes, fn workingtime ->
+
+      start_time = ensure_naive_datetime(workingtime.start)
+      end_time = ensure_naive_datetime(workingtime.end)
+
+      night_start_time = NaiveDateTime.new(start_time.year, start_time.month, start_time.day, 21, 0, 0) |> elem(1)
+      night_end_time = NaiveDateTime.new(start_time.year, start_time.month, start_time.day + 1, 6, 0, 0) |> elem(1) # 6h du jour suivant
+
+      IO.inspect(start_time)
+      IO.inspect(end_time)
+
+      IO.inspect(night_start_time)
+      IO.inspect(night_end_time)
+
+      adjusted_start_time =
+        if NaiveDateTime.compare(start_time, night_start_time) == :lt do
+          night_start_time
+        else
+          start_time
+        end
+
+      IO.inspect("adjusted_start_time #{adjusted_start_time}")
+
+      adjusted_end_time =
+        if NaiveDateTime.compare(end_time, night_end_time) == :gt do
+          night_end_time
+        else
+          end_time
+        end
+
+      IO.inspect("adjusted_end_time #{adjusted_end_time}")
+
+      total_night_seconds =
+        if NaiveDateTime.compare(adjusted_start_time, adjusted_end_time) == :lt do
+          NaiveDateTime.diff(adjusted_end_time, adjusted_start_time)
+        else
+          0
+        end
+
+      IO.inspect(("total_night_seconds #{total_night_seconds}"))
+
+    end)
+  end
+
+  defp ensure_naive_datetime(datetime) do
+    case datetime do
+      %NaiveDateTime{} -> datetime
+      %DateTime{} -> DateTime.to_naive(datetime)
+      _ -> raise "Unsupported datetime format"
+    end
+  end
+
+
+  # Calcule le temps travaillé par jour en secondes pour chaque jour de la période
+  def calc_chart_2(workingtimes) do
+
     all_times_worked = Enum.map(workingtimes, fn(workingtime) ->
 
       day = NaiveDateTime.to_date(workingtime.start)
@@ -80,45 +137,42 @@ defmodule ApiWeb.ChartManagerController do
 
 
   # TODO: Implémenter la récupération des jours fériés pour améliorer la précision du calcul du ratio de présence de l'employé
-  def calc_donut_chart(start_date, end_date, workingtimes) do
+  def calc_chart_1(start_date, end_date, workingtimes) do
 
-    # Ici c'est toutes les dates entre start_date et end_date
     all_dates = Enum.map(0..Date.diff(end_date, start_date), fn n ->
       Date.add(start_date, n)
     end)
 
-    # Ensuite on choppe les week-ends qui sont, d'après la fonction day_of_week, l'index 6 et 7
     weekends = Enum.filter(all_dates, fn date ->
       day_of_week = Date.day_of_week(date)
       day_of_week == 6 or day_of_week == 7
     end)
 
-    # Puis on choppe les jours où l'employé a travaillé
     worked_dates = workingtimes |> Enum.map(fn %{start: start_time} ->
         NaiveDateTime.to_date(start_time)
       end) |> Enum.uniq()
 
-    # On filtre les jours travaillés pour garder que ceux qui ne sont pas des week-ends
     days_worked = Enum.filter(all_dates, fn date ->
       date in worked_dates and not (date in weekends)
     end)
 
-    %{
-      days_worked: %{
+    data = [
+      %{
         name: "Jours travaillés",
-        description: "Correspond au nombre de jours où l'employé a travaillé",
         value: length(days_worked)
       },
-      days_not_worked: %{
+      %{
         name: "Jours non travaillés",
-        description: "Correspond au nombre de jours où l'employé n'a pas travaillé ou a été absent (week-ends non inclus)",
         value: length(all_dates) - length(days_worked) - length(weekends)
       },
-      ratio: %{
+      %{
         name: "Ratio",
-        description: "Pourcentage de jours travaillés par rapport au nombre total de jours dans la période",
         value: ((length(days_worked) / (length(all_dates) - length(weekends))) * 100) |> Float.ceil(2)
       }
+    ]
+
+    %{
+      data: data
     }
 
   end
