dcb540b5f37dbce570af9e7d2905376691f3532d
Torahime3 <nathan.dulac@epitech.eu>
2024-10-24 14:09:11 +0200

    feat(chartmanager): add a prototype to get the average start and end hours

--- backend/lib/api_web/controllers/chartmanager_controller.ex
+++ backend/lib/api_web/controllers/chartmanager_controller.ex
@@ -28,16 +28,17 @@ defmodule ApiWeb.ChartManagerController do
 
           chart_1_data = calc_chart_1(start_date, end_date, workingtimes)
           chart_2_data = calc_chart_2(workingtimes)
-
           chart_3_data = calc_chart_3(workingtimes)
+          chart_4_data = calc_chart_4(workingtimes)
 
           response = %{
             start_date: DateTime.to_iso8601(start_date),
             end_date: DateTime.to_iso8601(end_date),
             total_days: Date.diff(end_date, start_date),
-            chart_1: chart_1_data,
-            chart_2: chart_2_data,
-            chart_3: chart_3_data
+            # chart_1: chart_1_data,
+            # chart_2: chart_2_data,
+            # chart_3: chart_3_data,
+            chart_4: chart_4_data
           }
 
           conn
@@ -60,6 +61,47 @@ defmodule ApiWeb.ChartManagerController do
 
   end
 
+  #Calculer le temps moyen d'arrivée au travail et le temps moyen de fin de travail
+  def calc_chart_4(workingtimes) do
+
+    start_times = Enum.map(workingtimes, fn workingtime ->
+      ensure_naive_datetime(workingtime.start).hour * 3600 +
+      ensure_naive_datetime(workingtime.start).minute * 60 +
+      ensure_naive_datetime(workingtime.start).second
+    end)
+
+    end_times = Enum.map(workingtimes, fn workingtime ->
+      ensure_naive_datetime(workingtime.'end').hour * 3600
+      ensure_naive_datetime(workingtime.'end').minute * 60
+      ensure_naive_datetime(workingtime.'end').second
+    end)
+
+    average_start_time_seconds = Enum.sum(start_times) / Enum.count(start_times)
+    average_end_time_seconds = Enum.sum(end_times) / Enum.count(end_times)
+
+    %{
+      description: "Temps moyen d'arrivée au travail et temps moyen de fin de travail",
+      data: [
+        %{
+          name: "Temps moyen d'arrivée au travail",
+          value: average_start_time_seconds
+        },
+        %{
+          name: "Temps moyen de fin de travail",
+          value: average_end_time_seconds
+        }
+      ]
+    }
+
+  end
+
+  defp seconds_to_time(total_seconds) do
+    hours = div(total_seconds, 3600)
+    minutes = div(rem(total_seconds, 3600), 60)
+    seconds = rem(total_seconds, 60)
+    {hours, minutes, seconds}
+  end
+
 
   # Calculer le temps travaillé de nuit en secondes (horaires comptées de 21h à 6h)
   def calc_chart_3(workingtimes) do
@@ -77,21 +119,12 @@ defmodule ApiWeb.ChartManagerController do
             NaiveDateTime.new!(next_day.year, next_day.month, next_day.day, 6, 0, 0)
       end
 
-      # IO.puts("--------------------")
-      # IO.inspect("start_time #{start_time}")
-      # IO.inspect("end_time #{end_time}")
-
-      # IO.inspect("night_start_time #{night_start_time}")
-      # IO.inspect("night_end_time #{night_end_time}")
-
       adjusted_start_time =
         if NaiveDateTime.compare(start_time, night_start_time) == :lt do
           night_start_time
         else
           start_time
         end
-
-      # IO.inspect("adjusted_start_time #{adjusted_start_time}")
 
       adjusted_end_time =
         if NaiveDateTime.compare(end_time, night_end_time) == :gt do
@@ -100,7 +133,6 @@ defmodule ApiWeb.ChartManagerController do
           end_time
         end
 
-      # IO.inspect("adjusted_end_time #{adjusted_end_time}")
 
       total_night_seconds =
         if NaiveDateTime.compare(adjusted_start_time, adjusted_end_time) == :lt do
@@ -109,7 +141,6 @@ defmodule ApiWeb.ChartManagerController do
           0
         end
 
-      # IO.inspect(("total_night_seconds #{total_night_seconds}"))
       %{start_date: start_time, end_date: end_time, total_night_seconds: total_night_seconds}
 
     end)
