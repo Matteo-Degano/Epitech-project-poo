1101f536191d985aeb1d4c8d9d688d4d4c518bf6
AymaneHinane <aymane.hinane@epitech.eu>
2024-10-24 13:49:46 +0200

    working time table diplay data

--- react-native-app/app/(tabs)/Workingtimes.tsx
+++ react-native-app/app/(tabs)/Workingtimes.tsx
@@ -3,10 +3,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useEffect, useState } from 'react';
 import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-native';
 import { fetchData } from '../service/api';
+import WorkingTimeTable from '@/components/WorkingTimeTable';
+import { PaperProvider } from 'react-native-paper';
 
 
 
-interface workingtimeModel {
+export interface workingtimeModel {
     id: number
     start:string 
     end:string 
@@ -41,7 +43,8 @@ const Workingtimes = ()=>{
         console.log(response);
 
         if(response){
-            console.log(response.data);
+            //console.log(response.data.data);
+            setWorkingTimeData(response.data.data)
         }
     }
 
@@ -51,17 +54,27 @@ const Workingtimes = ()=>{
 
     useEffect(()=>{
        
-        LoadWorkingTime();
+        console.log("userId");
+        if(userId){
+           LoadWorkingTime();
+        }
 
     },[userId])
+
+    useEffect(()=>{
+        console.log("-----------------");
+        console.log(workingTimeData);
+    },[workingTimeData])
   
       
 
     return (
-        <Text>
-            Workingtimes
-        </Text>
-    )
+       
+        <View>
+            {workingTimeData.length > 0 && <WorkingTimeTable  workingTimeData={workingTimeData} />}
+        </View>
+    //      <PaperProvider></PaperProvider>
+      )
 }
 
 
