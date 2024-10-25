a77bbd5d9cf7deea559b31476afaac8260273ee7
Eva Ducéré <evaducere@gmail.com>
2024-10-25 16:59:32 +0200

    Replace logout button with a dropdown with account options

--- frontend/src/components/navigation/NavbarComponent.vue
+++ frontend/src/components/navigation/NavbarComponent.vue
@@ -3,12 +3,15 @@ import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuLink,
-  NavigationMenuList
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuContent
 } from "@/components/ui/navigation-menu"
 import ClockManager from "../ClockManager.vue"
 import { useAuthStore } from "@/stores/auth.store"
-import { House, Clock, Users, ChartSpline, LogOut } from "lucide-vue-next"
+import { House, Clock, Users, ChartSpline, LogOut, Settings } from "lucide-vue-next"
 import { useRoute, useRouter } from "vue-router"
+import DeleteAccount from "../users/DeleteAccount.vue"
 
 const authStore = useAuthStore()
 const route = useRoute()
@@ -40,44 +43,56 @@ const admin = 4
             </NavigationMenuLink>
           </router-link>
         </NavigationMenuItem>
-
+        
         <NavigationMenuItem>
           <router-link
-            to="/working-times"
-            :class="['nav-link', { 'text-primary': isActive('/working-times') }]"
+          to="/working-times"
+          :class="['nav-link', { 'text-primary': isActive('/working-times') }]"
           >
-            <NavigationMenuLink as="div">
-              <div class="flex gap-1 items-center"><Clock :size="18" /> Working Hours</div>
-            </NavigationMenuLink>
-          </router-link>
-        </NavigationMenuItem>
-
-        <NavigationMenuItem v-if="role === admin || role === generalManager">
-          <router-link to="/users" :class="['nav-link', { 'text-primary': isActive('/users') }]">
-            <NavigationMenuLink as="div">
-              <div class="flex gap-1 items-center"><Users :size="18" /> Users</div>
-            </NavigationMenuLink>
-          </router-link>
-        </NavigationMenuItem>
-
-        <NavigationMenuItem>
-          <router-link to="/charts" :class="['nav-link', { 'text-primary': isActive('/charts') }]">
-            <NavigationMenuLink as="div">
-              <div class="flex gap-1 items-center"><ChartSpline :size="18" /> Charts</div>
-            </NavigationMenuLink>
-          </router-link>
-        </NavigationMenuItem>
-
-        <NavigationMenuItem>
-          <NavigationMenuLink :class="['nav-link2', 'text-red-600']">
-            <button @click="logoutHandler">
-              <div class="flex gap-1 items-center"><LogOut :size="18" /> Logout</div>
-            </button>
+          <NavigationMenuLink as="div">
+            <div class="flex gap-1 items-center"><Clock :size="18" /> Working Hours</div>
           </NavigationMenuLink>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-
-      <ClockManager />
-    </div>
-  </NavigationMenu>
+        </router-link>
+      </NavigationMenuItem>
+      
+      <NavigationMenuItem v-if="role === admin || role === generalManager">
+        <router-link to="/users" :class="['nav-link', { 'text-primary': isActive('/users') }]">
+          <NavigationMenuLink as="div">
+            <div class="flex gap-1 items-center"><Users :size="18" /> Users</div>
+          </NavigationMenuLink>
+        </router-link>
+      </NavigationMenuItem>
+      
+      <NavigationMenuItem>
+        <router-link to="/charts" :class="['nav-link', { 'text-primary': isActive('/charts') }]">
+          <NavigationMenuLink as="div">
+            <div class="flex gap-1 items-center"><ChartSpline :size="18" /> Charts</div>
+          </NavigationMenuLink>
+        </router-link>
+      </NavigationMenuItem>
+      
+      <NavigationMenuItem>
+        <NavigationMenuTrigger class="p-0 font-normal text-base	hover:text-black hover:bg-white">
+          <div class="flex gap-1 items-center"><Settings :size="18" /> Account</div>
+        </NavigationMenuTrigger>
+        <NavigationMenuContent>
+          <ul class="w-52 p-4 grid gap-3">
+            <li>
+              <NavigationMenuLink class="text-red-600">
+                <button @click="logoutHandler">
+                  <div class="flex gap-1 items-center"><LogOut :size="18" /> Logout</div>
+                </button>
+              </NavigationMenuLink>
+            </li>
+            <li>
+              <DeleteAccount/>
+            </li>
+          </ul>
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+    </NavigationMenuList>
+    
+    <ClockManager />
+  </div>
+</NavigationMenu>
 </template>
