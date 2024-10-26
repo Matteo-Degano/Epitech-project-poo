a7df90b90519fc5be6a774aa75405eec725b95b8
Eva Ducéré <evaducere@gmail.com>
2024-10-26 15:46:50 +0200

    Add card with user data on homepage

---
+++ frontend/src/components/homepage/UserInfo.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { useAuthStore } from "@/stores/auth.store"
+
+const authStore = useAuthStore()
+
+function idToStringRole(id: number) {
+    switch (id) {
+        case 1:
+        return "Employee"
+        case 2:
+        return "Manager"
+        case 3:
+        return "General manager"
+        case 4:
+        return "Admin"
+        default:
+        return "Unknown"
+    }
+}
+
+console.log(authStore.user)
+</script>
+
+<template>
+    <div class="border-primary border-2 rounded p-4 w-1/3">
+        <p>User : {{authStore.user.username}}</p>
+        <p>Email : {{authStore.user.email}}</p>
+        <p>Role : {{idToStringRole(authStore.user.role_id)}}</p>
+        <p v-if="authStore.user.teams.length">Teams : {{ authStore.user.teams.map(team => team.name).join(', ') }}</p>
+    </div>
+</template>
