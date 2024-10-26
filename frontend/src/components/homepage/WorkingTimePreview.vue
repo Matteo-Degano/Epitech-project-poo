3037bb863e1d275a3e8f2aed959438680ae2b91e
Eva Ducéré <evaducere@gmail.com>
2024-10-26 16:59:17 +0200

    Refactor homepage view, fetch data in view instead of components

--- frontend/src/components/homepage/WorkingTimePreview.vue
+++ frontend/src/components/homepage/WorkingTimePreview.vue
@@ -1,39 +1,27 @@
 <script setup lang="ts">
-import { h, ref, onMounted} from "vue"
+import { h, ref } from "vue"
 import { type ColumnDef } from "@tanstack/vue-table"
-import { Button } from "@/components/ui/button"
 import { fetchData } from "@/services/api"
 import DataTable from "@/components/data-table/DataTable.vue"
 import { formatDateTime } from "@/utils/dateFormat"
 import type { WorkingTimeType } from "@/types/api.type"
 import { useAuthStore } from "@/stores/auth.store"
-import { useRoute } from "vue-router"
 
 const useAuth = useAuthStore()
 const workingTimeData = ref<WorkingTimeType[]>([])
-const isLoading = ref(true)
-const route = useRoute()
-const isActive = (path: string) => route.path === path
 
 const props = defineProps({
-  startTime: Date,
-  endTime: Date
+    data: Array,
 })
 
 const fetchWorkingTimes = async () => {
     try {
         const response = await fetchData("GET", `/workingtime/${useAuth.user.id}?start=${props.startTime}&end=${props.endTime}`)
-        console.log(response.data)
         workingTimeData.value = response.data
     } catch (err: any) {
         console.log(err)
     }
 }
-
-onMounted(async () => {
-    await fetchWorkingTimes()
-    isLoading.value = false
-})
 
 const columns: ColumnDef<WorkingTimeType>[] = [
     {
@@ -54,20 +42,14 @@ const columns: ColumnDef<WorkingTimeType>[] = [
 
 <template>
     <div class="flex flex-col gap-2 w-full">
-        <div v-if="isLoading" class="flex justify-center items-center h-full">
-            <p>Loading...</p>
-        </div>
-        <div v-else class="flex flex-col gap-1 w-full">
+        <div class="flex flex-col gap-1 w-full">
             <h2 class="font-bold">Latest working times</h2>
             <DataTable
             @refresh="fetchWorkingTimes"
             :columns="columns"
-            :data="workingTimeData"
+            :data="props.data"
             :filters="[]"
             />
-            <router-link to="/working-times" :class="[{ 'text-primary': isActive('/working-times')}, , 'self-end']">
-                <Button >View all</Button>
-            </router-link>
         </div>
     </div>
 </template>
