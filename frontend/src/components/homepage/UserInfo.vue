70d6cc03fcff03f00e06f81be4dae0485e009def
Matteo Degano <mdegano.webdev@gmail.com>
2024-10-27 15:37:13 +0100

    fix: ui fix on welcome card in homepage

--- frontend/src/components/homepage/UserInfo.vue
+++ frontend/src/components/homepage/UserInfo.vue
@@ -4,28 +4,34 @@ import { useAuthStore } from "@/stores/auth.store"
 const authStore = useAuthStore()
 
 function idToStringRole(id: number) {
-    switch (id) {
-        case 1:
-        return "Employee"
-        case 2:
-        return "Manager"
-        case 3:
-        return "General manager"
-        case 4:
-        return "Admin"
-        default:
-        return "Unknown"
-    }
+  switch (id) {
+    case 1:
+      return "Employee"
+    case 2:
+      return "Manager"
+    case 3:
+      return "General manager"
+    case 4:
+      return "Admin"
+    default:
+      return "Unknown"
+  }
 }
 
 console.log(authStore.user)
 </script>
 
 <template>
-    <div class="border-primary border-4 rounded p-4 w-1/3 flex flex-col justify-between gap-10 bg-[#faf6ff]">
-        <p>Welcome <span class="font-bold">{{ authStore.user.username }}</span> !</p>
-        <p><span class="font-bold">Email :</span> {{authStore.user.email}}</p>
-        <p><span class="font-bold">Role :</span> {{idToStringRole(authStore.user.role_id)}}</p>
-        <p v-if="authStore.user.teams.length"><span class="font-bold">Teams :</span> {{ authStore.user.teams.map(team => team.name).join(', ') }}</p>
-    </div>
+  <div class="border-primary border-4 rounded p-4 w-1/3 flex flex-col gap-4 bg-[#faf6ff]">
+    <h3 class="text-xl pb-4">Welcome !</h3>
+    <p>
+      <span class="font-bold">Username :</span> <span>{{ authStore.user.username }}</span> !
+    </p>
+    <p><span class="font-bold">Email :</span> {{ authStore.user.email }}</p>
+    <p><span class="font-bold">Role :</span> {{ idToStringRole(authStore.user.role_id) }}</p>
+    <p v-if="authStore.user.teams.length">
+      <span class="font-bold">Teams :</span>
+      {{ authStore.user.teams.map((team) => team.name).join(", ") }}
+    </p>
+  </div>
 </template>
