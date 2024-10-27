d114fa3c969d4f9cf8a0d012da251245490c9cf6
Eva Ducéré <evaducere@gmail.com>
2024-10-27 11:02:45 +0100

    Update user creation validation schema

--- frontend/src/components/users/UserModal.vue
+++ frontend/src/components/users/UserModal.vue
@@ -28,11 +28,11 @@ type Team = {
 }
 
 type User = {
-    id: number
-    username: string
-    email: string
-    role_id: number
-    teams: Team[]
+  id: number
+  username: string
+  email: string
+  role_id: number
+  teams: Team[]
 }
 
 const props = defineProps<{
@@ -88,18 +88,14 @@ const submitForm = async (body: any) => {
 }
 
 const selectedTeams = ref<string[]>([])
-const selectedRole = ref<number>()
-
-if(props.mode === "update") {
-  selectedTeams.value = props.data.teams.map(team => team.id.toString())
-  selectedRole.value = props.data.role_id
-}
 
 const initialValues = {
   username: props.mode === "update" ? props.data.username : "",
   email: props.mode === "update" ? props.data.email : "",
   password: "",
   confirmPassword: "",
+  teams: props.mode === "update" ? props.data.teams.map(team => team.id.toString()) : [],
+  role: props.mode === "update" ? props.data.role_id : 1
 }
 
 const onSubmit = (values) => {
@@ -107,8 +103,8 @@ const onSubmit = (values) => {
     username: values.username,
     email: values.email,
     password: values.password,
-    team_ids: selectedTeams.value,
-    role_id: selectedRole.value
+    team_ids: values.teams,
+    role_id: values.role
   }
   submitForm(body)
 }
@@ -133,13 +129,13 @@ const inputStyle="flex h-10 w-full rounded-md border border-input bg-background 
         <DialogDescription>
           {{
             props.mode === "create"
-              ? "Create an user here. Click save when you're done."
-              : "Make changes to an user here. Click save when you're done."
+            ? "Create an user here. Click save when you're done."
+            : "Make changes to an user here. Click save when you're done."
           }}
         </DialogDescription>
       </DialogHeader>
-
-      <Form v-slot="{errors}" :validation-schema="toTypedSchema(signupFormSchema)" @submit="onSubmit" class="flex flex-col w-full gap-6 p-2" :initialValues="initialValues">
+      
+      <Form v-slot="{errors}" :validation-schema="toTypedSchema(signupFormSchema)" @submit="onSubmit" class="flex flex-col w-full gap-6 p-2" :initialValues="initialValues" :validateOnMount="true">
         <label for="username" class="font-medium">Username</label>
         <Field type="text" name="username" :class="inputStyle"/>
         <ErrorMessage name="username" class="text-[#ef4444] font-medium"/>
@@ -147,30 +143,32 @@ const inputStyle="flex h-10 w-full rounded-md border border-input bg-background 
         <label for="email" class="font-medium">Email</label>
         <Field type="email" name="email" :class="inputStyle"/>
         <ErrorMessage name="email" class="text-[#ef4444] font-medium"/>
-
+        
         <label for="password" class="font-medium">Password</label>
         <Field type="password" name="password" :class="inputStyle"/>
         <ErrorMessage name="password" class="text-[#ef4444] font-medium"/>
-
+        
         <label for="confirmPassword" class="font-medium">Confirm password</label>
         <Field type="password" name="confirmPassword" :class="inputStyle"/>
         <ErrorMessage name="confirmPassword" class="text-[#ef4444] font-medium"/>
-
+        
         <label for="teams" class="font-medium">Select Teams</label>
         <div class="grid grid-cols-2 gap-4">
           <div v-for="team in props.teams" :key="team.id" class="flex items-center">
-            <input type="checkbox" :value="team.id" v-model="selectedTeams" class="mr-2" />
+            <Field type="checkbox" :value="team.id" v-model="selectedTeams" name="teams" class="mr-2" />
             <label>{{ team.name }}</label>
           </div>
         </div>
-
+        <ErrorMessage name="teams" class="text-[#ef4444] font-medium"/>
+        
         <label for="role" class="font-medium">Role</label>
-        <select v-model="selectedRole" :class="inputStyle">
+        <Field as="select" name="role" :class="inputStyle">
           <option value="1">Employee</option>
           <option value="2">Manager</option>
           <option value="3" v-if="authStore.user.role_id === 4">General manager</option>
           <option value="4" v-if="authStore.user.role_id === 4">Admin</option>
-        </select>
+        </Field>
+        <ErrorMessage name="role" class="text-[#ef4444] font-medium"/>
         
         <DialogClose as-child>
           <Button class="w-auto ml-auto" type="submit" :disabled="Object.keys(errors).length > 0"> {{ props.mode === "create" ? "Create": "Update" }} </Button>
