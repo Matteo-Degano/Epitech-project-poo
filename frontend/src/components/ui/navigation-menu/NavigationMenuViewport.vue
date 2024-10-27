30ea840e5dd8f9fac945bd8d489fce9a3c8bdfbc
Matteo Degano <mdegano.webdev@gmail.com>
2024-10-27 17:21:22 +0100

    fix : issue in dropdown navbar depending on the user role

--- frontend/src/components/ui/navigation-menu/NavigationMenuViewport.vue
+++ frontend/src/components/ui/navigation-menu/NavigationMenuViewport.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-import { cn } from '@/utils'
+import { useAuthStore } from "@/stores/auth.store"
+import { cn } from "@/utils"
 import {
   NavigationMenuViewport,
   type NavigationMenuViewportProps,
-  useForwardProps,
-} from 'radix-vue'
-import { computed, type HTMLAttributes } from 'vue'
+  useForwardProps
+} from "radix-vue"
+import { computed, type HTMLAttributes } from "vue"
 
-const props = defineProps<NavigationMenuViewportProps & { class?: HTMLAttributes['class'] }>()
+const authStore = useAuthStore()
+const role = authStore.getRole
+
+const expend = role === 3 || role === 4
+
+const props = defineProps<NavigationMenuViewportProps & { class?: HTMLAttributes["class"] }>()
 
 const delegatedProps = computed(() => {
   const { class: _, ...delegated } = props
@@ -19,13 +25,13 @@ const forwardedProps = useForwardProps(delegatedProps)
 </script>
 
 <template>
-  <div class="absolute left-[330px] top-full flex justify-center">
+  <div :class="['absolute top-full flex justify-center', expend ? 'left-[500px]' : 'left-[330px]']">
     <NavigationMenuViewport
       v-bind="forwardedProps"
       :class="
         cn(
           'origin-top-center relative mt-1.5 h-[--radix-navigation-menu-viewport-height] w-full overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[--radix-navigation-menu-viewport-width]',
-          props.class,
+          props.class
         )
       "
     />
