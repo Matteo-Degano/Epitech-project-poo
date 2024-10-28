867e12e945f81c1ad49342efa5dd2805dbf7aabc
Matteo Degano <mdegano.webdev@gmail.com>
2024-10-28 11:59:40 +0100

    fix: height chart in homepage

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import UserInfo from "@/components/homepage/UserInfo.vue";
-import WorkingTimePreview from "@/components/homepage/WorkingTimePreview.vue";
-import Separator from "@/components/ui/separator/Separator.vue";
-import { useToast } from "@/components/ui/toast/use-toast";
-import { useAuthStore } from "@/stores/auth.store";
-import { onMounted, ref } from "vue";
-import { fetchData } from "@/services/api";
-import HoursWorkedChart from "@/components/charts/HoursWorkedChart.vue";
-import Spinner from "@/components/Spinner.vue";
+import UserInfo from "@/components/homepage/UserInfo.vue"
+import WorkingTimePreview from "@/components/homepage/WorkingTimePreview.vue"
+import Separator from "@/components/ui/separator/Separator.vue"
+import { useToast } from "@/components/ui/toast/use-toast"
+import { useAuthStore } from "@/stores/auth.store"
+import { onMounted, ref } from "vue"
+import { fetchData } from "@/services/api"
+import HoursWorkedChart from "@/components/charts/HoursWorkedChart.vue"
+import Spinner from "@/components/Spinner.vue"
 
 const authStore = useAuthStore()
 const isLoading = ref(true)
@@ -17,11 +17,14 @@ const workingTimeData = ref(null)
 const dayTime = ref(null)
 const currentDate = new Date()
 const lastWeek = new Date()
-lastWeek.setDate(lastWeek.getDate() - 7);
+lastWeek.setDate(lastWeek.getDate() - 7)
 
 const fetchWorkingTimes = async () => {
   try {
-    const response = await fetchData("GET", `/workingtime/${authStore.user.id}?start=${lastWeek.toISOString()}&end=${currentDate.toISOString()}`)
+    const response = await fetchData(
+      "GET",
+      `/workingtime/${authStore.user.id}?start=${lastWeek.toISOString()}&end=${currentDate.toISOString()}`
+    )
     workingTimeData.value = response.data
   } catch (err: any) {
     console.log(err)
@@ -30,7 +33,10 @@ const fetchWorkingTimes = async () => {
 
 const fetchChart = async () => {
   try {
-    const response = await fetchData("GET", `/chartmanager/${authStore.user.id}?start=${lastWeek.toISOString()}&end=${currentDate.toISOString()}`)
+    const response = await fetchData(
+      "GET",
+      `/chartmanager/${authStore.user.id}?start=${lastWeek.toISOString()}&end=${currentDate.toISOString()}`
+    )
     dayTime.value = response.data.chart_2
   } catch (err) {
     error.value = err.message || "Error fetching data"
@@ -47,21 +53,20 @@ onMounted(async () => {
   console.log(workingTimeData.value)
   console.log(dayTime.value)
 })
-
 </script>
 
 <template>
   <div v-if="isLoading" class="flex justify-center items-center h-full">
-    <Spinner/>
+    <Spinner />
   </div>
   <div v-else class="flex flex-col justify-around h-full gap-10">
-    <div class="flex gap-8">
-      <UserInfo/>
-      <WorkingTimePreview :data="workingTimeData"/>
+    <div class="flex gap-8 h-1/2">
+      <UserInfo />
+      <WorkingTimePreview :data="workingTimeData" />
     </div>
-    <Separator/>
-    <div>
-      <HoursWorkedChart v-if="dayTime" :data="dayTime" class="w-full h-72"/>
+    <Separator />
+    <div class="w-full h-96">
+      <HoursWorkedChart v-if="dayTime" :data="dayTime" />
     </div>
   </div>
 </template>
